627fdfe6904dfd92a4f52ff1533e2d6459526bbf
GW <gwsounddsg@gmail.com>
2020-11-22 21:12:14 -0800

    add two methods and a class to the cpp file

--- lib/my-lib.cpp
+++ lib/my-lib.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hellow World" << std::endl;
-    return 0;
+int foo() {
+    return 8;
+}
+
+int bar(int x) {
+    return x + 42;
+}
+
+
+class ClassA {
+
+public:
+    ClassA();
+    ~ClassA();
+    int Add(int a, int b);
+};
+
+ClassA::ClassA() {}
+ClassA::~ClassA() {}
+
+int ClassA::Add(int a, int b) {
+    return a + b;
 }
