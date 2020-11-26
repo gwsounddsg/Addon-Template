9448b7a93df8388e0a1cdce86d79255edb774b6b
GW <gwsounddsg@gmail.com>
2020-11-26 11:44:51 -0800

    add method AddThree

--- addon/lib/myLib.cpp
+++ addon/lib/myLib.cpp
@@ -3,18 +3,34 @@
 #include <iostream>
 
 using namespace std;
-using namespace Nan;
 using namespace v8;
 
 
+// Basic no param method that prints
 NAN_METHOD(MyPrint) {
     std::cout << "Hello there JavaScript" << std::endl;
 }
 
+// Receives a number adds 3 and returns
+NAN_METHOD(AddThree) {
+    if (info.Length() != 1) {
+        Nan::ThrowTypeError("Wrong number of arguments");
+        return;
+    }
+
+    Nan::Maybe<double> value = Nan::To<double>(info[0]);
+    Local<Number> newValue = Nan::New(value.FromJust() + 42);
+    info.GetReturnValue().Set(newValue);
+}
+
 NAN_MODULE_INIT(Init) {
     Nan::Set(target,
-        New<String>("myPrint").ToLocalChecked(),
-        GetFunction(New<FunctionTemplate>(MyPrint)).ToLocalChecked());
+        Nan::New<String>("myPrint").ToLocalChecked(),
+        Nan::GetFunction(Nan::New<FunctionTemplate>(MyPrint)).ToLocalChecked());
+
+    Nan::Set(target,
+        Nan::New<String>("addThree").ToLocalChecked(),
+        Nan::GetFunction(Nan::New<FunctionTemplate>(AddThree)).ToLocalChecked());
 }
 
 NODE_MODULE(my_cpp_addon, Init)
