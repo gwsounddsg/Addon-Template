a471c69a1061b3b1c7b6b804d18befff31e81254
GW <gwsounddsg@gmail.com>
2020-11-27 08:36:35 -0800

    calls enumValue and currentColor

--- addon/main.js
+++ addon/main.js
@@ -9,3 +9,15 @@ console.log(addon.addInts(2, 3));
 console.log(addon.addInts(1.2, 4));
 
 console.log(`True is the new ${addon.flipBool(true)}`);
+
+const Colors = {
+    Red: 0,
+    Green: 1,
+    Blue: 2,
+};
+
+console.log(`Green has a value of ${addon.enumValue(Colors.Green)}`);
+const myColor = addon.currentColor();
+console.log(
+    `Current color: ${Object.keys(Colors).find((key) => Colors[key] === 2)}`
+);
